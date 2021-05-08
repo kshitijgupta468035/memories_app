5e81d28c527c50a6677063a605f12e0c248d6f8d
kg468035 <kshitij.gupta1_cs18@gla.ac.in>
2021-05-08 15:48:22 +0530

    Add getPosts method in posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,4 +5,14 @@ import PostMessage from '../models/postMessage.js';
 
 const router = express.Router();
 
+export const getPosts = async (req, res) => {
+	try {
+		const postMessages = await PostMessage.find();
+
+		res.status(200).json(postMessages);
+	} catch (error) {
+		res.status(404).json({ message: error.message });
+	}
+};
+
 export default router;
