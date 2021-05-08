d0a9fe9d2bd98302ab6cb3f20c9975455bc54568
kg468035 <kshitij.gupta1_cs18@gla.ac.in>
2021-05-08 17:35:55 +0530

    Add handleSubmit method

--- client/src/components/Form/From.js
+++ client/src/components/Form/From.js
@@ -22,6 +22,18 @@ const Form = ({ currentId, setCurrentId }) => {
 		setCurrentId(0);
 		setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
 	};
+    
+    const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		if (currentId === 0) {
+			dispatch(createPost(postData));
+			clear();
+		} else {
+			dispatch(updatePost(currentId, postData));
+			clear();
+		}
+	};
 
 	return (
 		<Paper className={classes.paper}>
