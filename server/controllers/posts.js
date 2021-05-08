3c518fac3468c2fdc977dcb1204b26152eb73425
kg468035 <kshitij.gupta1_cs18@gla.ac.in>
2021-05-08 15:49:37 +0530

    Add getPost method in posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -15,4 +15,16 @@ export const getPosts = async (req, res) => {
 	}
 };
 
+export const getPost = async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		const post = await PostMessage.findById(id);
+
+		res.status(200).json(post);
+	} catch (error) {
+		res.status(404).json({ message: error.message });
+	}
+};
+
 export default router;
